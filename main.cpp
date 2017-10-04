8d1769622bcd2920cd92f75d4ff10a3e80c79375
Christian Perez <cpere034@hammer.cs.ucr.edu>
2017-10-03 19:46:01 -0700

    Solved merge conflict between userinput and master branches

--- main.cpp
+++ main.cpp
@@ -6,10 +6,9 @@ using namespace std;
 int main()
 {
 	string name;
-
 	cout << "What is your name?" << endl;
 	cin >> name;
-	cout < "Hello " << name << "!" << endl;
+	cout "Hello "  << "!" << endl;
 
 	return 0;
 }
